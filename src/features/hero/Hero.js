3c5411a43e05fcc23f093ead78b5a6f71859082e
Kevin Carter <console.logkevin@gmail.com>
2025-07-31 01:19:46 -0400

    feat: build hero section UI

--- src/features/hero/Hero.js
+++ src/features/hero/Hero.js
@@ -1,10 +1,42 @@
 "use client";
 import styles from "./Hero.module.css";
+import Image from "next/image";
+import logo from "../../../public/hero-logo.png";
 
+// className={styles.hero}
 export function Hero() {
    return (
       <section className={styles.heroContainer}>
-         <h1>Hero</h1>
+         <div className={styles.heroLogoCase}>
+            <Image 
+               className={styles.heroLogo}
+               src={logo} 
+               alt="hero logo"
+               fill
+            ></Image>
+         </div>
+
+         <p className={styles.heroHeading}>Facing Eviction? You Still Have Options.</p>
+
+         <div className={styles.heroShowCase}>
+            <p className={styles.heroSubHeading}>
+               This website is not about just evictions this also informs how to inspect a 
+               property when buying and or renting to prevent complications and to educate 
+               people because knowledge is power. With our 30 years of experience in 
+               construction and building main-tenance, and our knowledge of the eviction 
+               process, we provide expert consultation to tenants, landlords, homeowners, 
+               realtors and investors on eviction mediation.
+            </p>
+
+            <button className={styles.heroButton}>Receive Your guide</button>
+         </div>
       </section>
    );
 }
+
+/*
+
+git add . && git commit -m "feat: build hero section UI" && git push origin feature/hero && git switch main && git pull && git merge feature/hero && git push && git switch feature/hero
+
+
+*/
